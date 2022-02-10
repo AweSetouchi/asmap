1dc840690259f42ee767ac77d22de18b2d6b1d57
saka <saka@netfort.gr.jp>
2022-02-10 20:59:04 +0900

    update views & config file

--- initialize.js
+++ initialize.js
@@ -3,11 +3,12 @@
 
 // Global Variable
 var Conf = {};					// Config Praams
+const GTAG = '<script async src="https://www.googletagmanager.com/gtag/js?id=';
 const LANG = (window.navigator.userLanguage || window.navigator.language || window.navigator.browserLanguage).substr(0, 2) == "ja" ? "ja" : "en";
 const FILES = [
 	"./baselist.html", "./data/config.json", './data/config-system.json', './data/config-activities.json',
-	'./data/overpass.json', `./data/marker.json`, `./data/category-${LANG}.json`, `data/listtable-${LANG}.json`,
-	`./data/glot-custom.json`, `data/glot-system.json`];
+	`./data/marker.json`, `./data/category-${LANG}.json`, `data/listtable-${LANG}.json`,
+	`./data/glot-custom.json`, `data/glot-system.json`, './data/overpass-system.json', `./data/overpass-custom.json`];
 const glot = new Glottologist();
 var gSheet = new GoogleSpreadSheet();
 var cMapmaker = new CMapMaker();
@@ -18,28 +19,24 @@ var modal_wikipedia = new modal_Wikipedia();
 var basic = new Basic();
 var leaflet = new Leaflet();
 
-
 // initialize
 console.log("Welcome to MapMaker.");
 window.addEventListener("DOMContentLoaded", function () {
 	let jqXHRs = [];
 	for (let key in FILES) { jqXHRs.push($.get(FILES[key])) };
 	$.when.apply($, jqXHRs).always(function () {
-		let arg = {}, basehtml = arguments[0][0];								// Get Menu HTML
-		for (let i = 1; i <= 7; i++) arg = Object.assign(arg, arguments[i][0]);	// Make Config Object
-		Object.keys(arg).forEach(key1 => {
-			Conf[key1] = {};
-			Object.keys(arg[key1]).forEach((key2) => Conf[key1][key2] = arg[key1][key2]);
-		});
-		Conf.category_keys = Object.keys(Conf.category);						// Make Conf.category_keys
-		glot.data = Object.assign(glot.data, arguments[8][0]);					// import glot data
-		glot.data = Object.assign(glot.data, arguments[9][0]);					// import glot data
-
+		let basehtml = arguments[0][0];								// Get Menu HTML
+		for (let i = 1; i <= 6; i++) Conf = Object.assign(Conf, arguments[i][0]);	// Make Config Object
+		Conf.category_keys = Object.keys(Conf.category);							// Make Conf.category_keys
+		glot.data = Object.assign(glot.data, arguments[7][0]);						// import glot data
+		glot.data = Object.assign(glot.data, arguments[8][0]);						// import glot data
+		Conf = Object.assign(Conf, arguments[9][0]);						// import OverPass
+		Conf.osm = Object.assign(Conf.osm, arguments[10][0].osm);						// import OverPass
 		window.onresize = winCont.window_resize;    // 画面サイズに合わせたコンテンツ表示切り替え
 		// document.title = glot.get("title");		// Title(no change / Google検索で日本語表示させたいので)
 		cMapmaker.init(basehtml);					// Mapmaker Initialize
 		if (Conf.google.Analytics !== "") {			// Google Analytics
-			let AnalyticsURL = '<script async src="https://www.googletagmanager.com/gtag/js?id=' + Conf.default.GoogleAnalytics + '"></script>';
+			let AnalyticsURL = GTAG + Conf.default.GoogleAnalytics + '"></script>';
 			document.getElementsByTagName('head').insertAdjacentHTML("beforeend", AnalyticsURL);
 			window.dataLayer = window.dataLayer || [];
 			function gtag() { dataLayer.push(arguments); };
